3bb16e1a2cecfac5422c0bcdb3817d748ec50f21
midhxn <midhunn311@gmail.com>
2022-12-27 23:45:29 +0530

    update

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const expresd = require('express')
-const dotenv = require('dotenv').config()
+const expresd = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app =express();
+
+app.listen(port,()=>console.log(`server on port${port}`));
