ff0fcf77b8d2e0627f4d182514ebce0b5b6db1dd
midhxn <midhunn311@gmail.com>
2022-12-27 23:55:25 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const expresd = require('express');
+const express = require('express');
 const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-const app =express();
+const app = express();
 
-app.listen(port,()=>console.log(`server on port${port}`));
+app.use('/openai', require('./routes/openaiRoutes'));
+
+app.listen(port, ()=>console.log(`server on port ${port}`));
